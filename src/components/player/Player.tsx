83c902a4b3db6add8baa83a2423acf978724c6cb
jeffvli <jeffvictorli@gmail.com>
2021-08-04 11:19:32 -0700

    change audio to react-h5-audio-player

--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -1,35 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import Sound from 'react-sound';
+import React, { createRef } from 'react';
+import { Helmet } from 'react-helmet-async';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import { useAppSelector, useAppDispatch } from '../../redux/hooks';
+import {
+  incrementCurrentIndex,
+  decrementCurrentIndex,
+  setIsLoading,
+  setIsLoaded,
+} from '../../redux/playQueueSlice';
 
-/* const useAudio = (url) => {
-  const [audio] = useState(new Audio(url));
-  const [playing, setPlaying] = useState(false);
+const Player = () => {
+  const playerRef = createRef<AudioPlayer>();
+  const playQueue = useAppSelector((state: any) => state.playQueue);
+  const dispatch = useAppDispatch();
 
-  const toggle = () => setPlaying(!playing);
+  const handleOnLoadStart = () => {
+    dispatch(setIsLoading());
+  };
 
-  useEffect(() => {
-    playing ? audio.play() : audio.pause();
-  }, [playing]);
+  const handleOnLoadedData = () => {
+    dispatch(setIsLoaded());
+  };
 
-  useEffect(() => {
-    audio.addEventListener('ended', () => setPlaying(false));
-    return () => {
-      audio.removeEventListener('ended', () => setPlaying(false));
-    };
-  }, []);
+  const handleOnClickNext = () => {
+    dispatch(incrementCurrentIndex());
+    console.log(playerRef?.current?.audio?.current);
+  };
 
-  return [playing, toggle];
-}; */
+  const handleOnClickPrevious = () => {
+    dispatch(decrementCurrentIndex());
+  };
 
-const Player = ({ url }) => {
+  const handleOnEnded = () => {
+    dispatch(incrementCurrentIndex());
+  };
+
   return (
-    <div>
-      <Sound
-        url={url}
-        playStatus={Sound.status.PLAYING}
-        playFromPosition={0}
-      ></Sound>
-    </div>
+    <>
+      {playQueue.entry[playQueue.currentIndex] && (
+        <Helmet>
+          <title>
+            {playQueue.entry[playQueue.currentIndex]?.title} — sonicD
+          </title>
+        </Helmet>
+      )}
+
+      <AudioPlayer
+        ref={playerRef}
+        src={playQueue.entry[playQueue.currentIndex]?.streamUrl}
+        autoPlay
+        showSkipControls
+        showFilledVolume
+        showJumpControls={false}
+        onPlay={() => console.log('onPlay')}
+        onPlaying={(e) => console.log(e)}
+        onClickNext={handleOnClickNext}
+        onClickPrevious={handleOnClickPrevious}
+        onEnded={handleOnEnded}
+        onLoadStart={handleOnLoadStart}
+        onLoadedData={handleOnLoadedData}
+      />
+    </>
   );
 };
 
--- src/redux/playQueueSlice.ts
+++ src/redux/playQueueSlice.ts
@@ -1,49 +1,89 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-type PlayQueueTypes = [
-  {
-    id: string;
-    album: string;
-    albumId: string;
-    artist: string;
-    bitRate: number;
-    contentType: string;
-    coverArt: string;
-    created: string;
-    duration: string;
-    genre: string;
-    index: number;
-    isDir: boolean;
-    isVideo: boolean;
-    parent: string;
-    path: string;
-    playCount: number;
-    size: number;
-    starred?: string;
-    streamUrl: string;
-    suffix: string;
-    title: string;
-    track: number;
-    type: string;
-    year: number;
-  }
-];
+interface Entry {
+  id: string;
+  album: string;
+  albumId: string;
+  artist: string;
+  bitRate: number;
+  contentType: string;
+  coverArt: string;
+  created: string;
+  duration: string;
+  genre: string;
+  index: number;
+  isDir: boolean;
+  isVideo: boolean;
+  parent: string;
+  path: string;
+  playCount: number;
+  size: number;
+  starred?: string;
+  streamUrl: string;
+  suffix: string;
+  title: string;
+  track: number;
+  type: string;
+  year: number;
+}
 
-const initialState: PlayQueueTypes[] = [];
+interface PlayQueue {
+  currentIndex: number;
+  volume: number;
+  isLoading: boolean;
+  entry: Entry[];
+}
+
+const initialState: PlayQueue = {
+  currentIndex: 0,
+  volume: 0.5,
+  isLoading: false,
+  entry: [],
+};
 
 const playQueueSlice = createSlice({
   name: 'nowPlaying',
   initialState,
   reducers: {
-    setPlayQueue: (state, action: PayloadAction<PlayQueueTypes>) => {
-      action.payload.map((entry: any) => state.push(entry));
+    incrementCurrentIndex: (state) => {
+      if (state.currentIndex <= state.entry.length) {
+        state.currentIndex += 1;
+      } else {
+        state.currentIndex = 0;
+      }
     },
-    appendPlayQueue: (state, action: PayloadAction<PlayQueueTypes>) => {
-      action.payload.map((entry: any) => state.push(entry));
+    decrementCurrentIndex: (state) => {
+      if (state.currentIndex >= 0) {
+        state.currentIndex -= 1;
+      }
+    },
+    setCurrentIndex: (state, action: PayloadAction<PlayQueue>) => {
+      state.currentIndex = action.payload.currentIndex;
+    },
+    setPlayQueue: (state, action: PayloadAction<Entry[]>) => {
+      state.currentIndex = 0;
+      action.payload.map((entry: any) => state.entry.push(entry));
+    },
+    appendPlayQueue: (state, action: PayloadAction<Entry[]>) => {
+      action.payload.map((entry: any) => state.entry.push(entry));
     },
     clearPlayQueue: () => initialState,
+    setIsLoading: (state) => {
+      state.isLoading = true;
+    },
+    setIsLoaded: (state) => {
+      state.isLoading = false;
+    },
   },
 });
 
-export const { setPlayQueue, clearPlayQueue } = playQueueSlice.actions;
+export const {
+  incrementCurrentIndex,
+  decrementCurrentIndex,
+  setCurrentIndex,
+  setPlayQueue,
+  clearPlayQueue,
+  setIsLoading,
+  setIsLoaded,
+} = playQueueSlice.actions;
 export default playQueueSlice.reducer;
