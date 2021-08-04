904570b9a69f70f201cb7e96bb8ed0a5e0ff8ebf
jeffvli <jeffvictorli@gmail.com>
2021-08-04 11:24:33 -0700

    remove unneeded

--- src/components/player/NowPlayingList.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NowPlayingList = () => {
-  return <div />;
-};
-
-export default NowPlayingList;
--- src/components/playlist/Playlist.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Playlist = () => {
-  return <div></div>;
-};
-
-export default Playlist;
