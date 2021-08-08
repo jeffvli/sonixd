50ce257c07796216124e4926679d1891313bd742
jeffvli <jeffvictorli@gmail.com>
2021-08-08 08:17:35 -0700

    add default styled component theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import { ThemeProvider } from 'styled-components';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './App';
 
+import { theme } from './styles/styled';
+
 render(
   <Provider store={store}>
-    <HelmetProvider>
-      <DndProvider backend={HTML5Backend}>
-        <App />
-      </DndProvider>
-    </HelmetProvider>
+    <ThemeProvider theme={theme}>
+      <HelmetProvider>
+        <DndProvider backend={HTML5Backend}>
+          <App />
+        </DndProvider>
+      </HelmetProvider>
+    </ThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
---
+++ src/styles/styled.ts
@@ -0,0 +1,3 @@
+export const theme = {
+  main: '#1179ac',
+};
