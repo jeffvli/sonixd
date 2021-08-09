661ded3775e92c5d885f579e36f6d2312ecb2107
jeffvli <jeffvictorli@gmail.com>
2021-08-08 19:35:38 -0700

    set divider to optional

--- src/components/layout/GenericPage.tsx
+++ src/components/layout/GenericPage.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Container, Header, Content, Divider } from 'rsuite';
 import '../../styles/GenericPage.global.css';
 
-const GenericPage = ({ header, children }: any) => {
+const GenericPage = ({ header, children, hideDivider }: any) => {
   return (
     <Container id="page" className="container__page">
       <Header className="page__header">{header}</Header>
-      <Divider />
+      {!hideDivider && <Divider />}
       <Content className="page__content">{children}</Content>
     </Container>
   );
