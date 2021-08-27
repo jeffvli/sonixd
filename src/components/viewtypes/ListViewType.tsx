066be6bf01f744dcdb26f6684d4dc7285c5be253
jeffvli <jeffvictorli@gmail.com>
2021-08-27 04:55:08 -0700

    remove experimental dnd

--- src/components/viewtypes/ListViewType.tsx
+++ src/components/viewtypes/ListViewType.tsx
@@ -3,7 +3,6 @@
 // Resize derived from @nimrod-cohen https://gitter.im/rsuite/rsuite?at=5e1cd3f165540a529a0f5deb
 import React, { useState, useEffect, useRef } from 'react';
 import { Table, DOMHelper } from 'rsuite';
-
 import { nanoid } from '@reduxjs/toolkit';
 import '../../styles/ListView.global.css';
 import { formatSongDuration } from '../../shared/utils';
@@ -38,11 +37,16 @@ const ListViewType = ({
   children,
   ...rest
 }: any) => {
+  // const [isDragging, setIsDragging] = useState(false);
+  // const [dragDirection, setDragDirection] = useState('down');
+  // const [dragSpeed, setDragSpeed] = useState('medium');
+
   const [height, setHeight] = useState(0);
   const [show, setShow] = useState(false);
   const [columns, setColumns] = useState(tableColumns);
 
   const { getHeight } = DOMHelper;
+  const tableRef = useRef<any>();
   const wrapperRef = useRef<HTMLDivElement>(null);
   const multiSelect = useAppSelector((state: any) => state.multiSelect);
   const playQueue = useAppSelector((state: any) => state.playQueue);
@@ -76,6 +80,40 @@ const ListViewType = ({
     });
   }, [getHeight]);
 
+  /* useEffect(() => {
+    let scrollDistance = 0;
+    switch (dragSpeed) {
+      case 'slow':
+        scrollDistance = 15;
+        break;
+      case 'medium':
+        scrollDistance = 30;
+        break;
+      case 'fast':
+        scrollDistance = 60;
+        break;
+      default:
+        scrollDistance = 15;
+        break;
+    }
+
+    if (isDragging) {
+      const interval = setInterval(() => {
+        const currentScroll = Math.abs(tableRef?.current.scrollY);
+        tableRef.current.scrollTop(
+          dragDirection === 'down'
+            ? currentScroll + scrollDistance
+            : dragDirection === 'up' && currentScroll - scrollDistance > 0
+            ? currentScroll - scrollDistance
+            : 0
+        );
+      }, 50);
+
+      return () => clearInterval(interval);
+    }
+    return () => clearInterval();
+  }, [dragDirection, dragSpeed, isDragging]); */
+
   return (
     <>
       {!show && <Loader />}
@@ -86,13 +124,116 @@ const ListViewType = ({
           handleManualClick={rest.handleManualClick}
         />
       )}
+
+      {/* <Button onClick={() => tableRef.current.scrollTop(16000)}>Scroll</Button> */}
+      {/* <Button onClick={() => console.log(tableRef.current)}>Info</Button> */}
+      {/* <div
+        style={{ position: 'absolute', left: '50%', top: '200px', zIndex: 1 }}
+      >
+        <div>Direction: {dragDirection}</div>
+        <div>IsDragging: {isDragging ? 'true' : 'false'}</div>
+        <div>DragSpeed: {dragSpeed}</div>
+      </div> */}
+
       <div
         className="table__container"
         style={{ flexGrow: 1 }}
         ref={wrapperRef}
       >
+        {/* <div
+          id="scroll-top"
+          style={{
+            position: 'absolute',
+            background: 'blue',
+            height: '15%',
+            width: '100%',
+            top: 0,
+            left: 0,
+            right: 0,
+            zIndex: isDragging ? 1 : undefined,
+          }}
+          onMouseEnter={() => {
+            console.log('panning down');
+            setIsDragging(true);
+            setDragDirection('up');
+          }}
+          onMouseLeave={() => {
+            console.log('stopped panning down');
+            setIsDragging(false);
+            setDragDirection('none');
+          }}
+        >
+          <div
+            id="scroll-top-fast"
+            style={{ height: 'calc(100% / 3)', background: 'green' }}
+            onMouseEnter={() => {
+              setDragSpeed('fast');
+            }}
+          />
+          <div
+            id="scroll-top-medium"
+            style={{ height: 'calc(100% / 3)', background: 'blue' }}
+            onMouseEnter={() => {
+              setDragSpeed('medium');
+            }}
+          />
+          <div
+            id="scroll-top-slow"
+            style={{ height: 'calc(100% / 3)', background: 'red' }}
+            onMouseEnter={() => {
+              setDragSpeed('slow');
+            }}
+          />
+        </div>
+        <div
+          id="scroll-bottom"
+          style={{
+            position: 'absolute',
+            background: 'red',
+            height: '15%',
+            width: '100%',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            zIndex: isDragging ? 1 : undefined,
+          }}
+          onMouseEnter={() => {
+            console.log('panning down');
+            setIsDragging(true);
+            setDragDirection('down');
+          }}
+          onMouseLeave={() => {
+            console.log('stopped panning down');
+            setIsDragging(false);
+            setDragDirection('none');
+          }}
+        >
+          <div
+            id="scroll-bottom-slow"
+            style={{ height: 'calc(100% / 3)', background: 'red' }}
+            onMouseEnter={() => {
+              setDragSpeed('slow');
+            }}
+          />
+          <div
+            id="scroll-bottom-medium"
+            style={{ height: 'calc(100% / 3)', background: 'blue' }}
+            onMouseEnter={() => {
+              setDragSpeed('medium');
+            }}
+          />
+          <div
+            id="scroll-bottom-fast"
+            style={{ height: 'calc(100% / 3)', background: 'green' }}
+            onMouseEnter={() => {
+              setDragSpeed('fast');
+            }}
+          />
+        </div> */}
+
         {show && (
           <Table
+            ref={tableRef}
             height={height}
             data={data}
             virtualized={virtualized}
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { ThemeProvider } from 'styled-components';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
@@ -15,13 +13,11 @@ const queryClient = new QueryClient();
 render(
   <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <DndProvider backend={HTML5Backend}>
-        <QueryClientProvider client={queryClient}>
-          <HelmetProvider>
-            <App />
-          </HelmetProvider>
-        </QueryClientProvider>
-      </DndProvider>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   </Provider>,
   document.getElementById('root')
