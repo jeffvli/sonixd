6dec738cbee7a3ca58da611f7ad864185780289e
jeffvli <jeffvictorli@gmail.com>
2021-09-05 16:44:21 -0700

    update default tests to include store mock

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,59 @@
 import React from 'react';
 import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import { Middleware, Dispatch, AnyAction } from 'redux';
+import configureMockStore from 'redux-mock-store';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-// Remove until I can resolve mocking settings into components
+const middlewares:
+  | Middleware<Record<string, unknown>, any, Dispatch<AnyAction>>[]
+  | undefined = [];
+const mockStore = configureMockStore(middlewares);
+
 describe('App', () => {
-  it('should render', () => {});
+  it('Should render with dark theme', () => {
+    const store = mockStore({
+      misc: {
+        theme: 'defaultDark',
+      },
+    });
+    expect(
+      render(
+        <Provider store={store}>
+          <App />
+        </Provider>
+      )
+    ).toBeTruthy();
+  });
+
+  it('Should render with light theme', () => {
+    const store = mockStore({
+      misc: {
+        theme: 'defaultLight',
+      },
+    });
+    expect(
+      render(
+        <Provider store={store}>
+          <App />
+        </Provider>
+      )
+    ).toBeTruthy();
+  });
+
+  it('Should render with no theme specified', () => {
+    const store = mockStore({
+      misc: {
+        theme: '',
+      },
+    });
+    expect(
+      render(
+        <Provider store={store}>
+          <App />
+        </Provider>
+      )
+    ).toBeTruthy();
+  });
 });
