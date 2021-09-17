4f8be8166901c820d6ad7d497b06a5e12ab16633
jeffvli <jeffvictorli@gmail.com>
2021-09-17 05:03:35 -0700

    update toast - allow any message - add title case for title - customize styling

--- src/components/shared/toast.ts
+++ src/components/shared/toast.ts
@@ -2,10 +2,10 @@ import { Notification } from 'rsuite';
 
 export const notifyToast = (
   type: 'info' | 'success' | 'warning' | 'error',
-  message: string
+  message: any
 ) => {
   Notification[type]({
-    title: type.toUpperCase(),
+    title: `${type.charAt(0).toUpperCase()}${type.slice(1)}`,
     description: message,
   });
 };
