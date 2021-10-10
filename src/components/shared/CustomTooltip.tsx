bc573c0d23d92dc9d0647da3d0fa9d75faf4f858
jeffvli <jeffvictorli@gmail.com>
2021-10-10 05:11:45 -0700

    Adjust tooltip styling to theme

--- src/components/shared/CustomTooltip.tsx
+++ src/components/shared/CustomTooltip.tsx
@@ -3,21 +3,22 @@ import { Tooltip, Whisper } from 'rsuite';
 import styled from 'styled-components';
 
 const StyledTooltip = styled(Tooltip)`
+  border: 1px #3c3f43 solid;
   .rs-tooltip-inner {
-    background-color: #000000;
-    color: #ffffff;
+    background-color: ${(props) => props.theme.primary.background};
+    color: ${(props) => props.theme.primary.text};
   }
 `;
 
 export const tooltip = (text: string) => <StyledTooltip>{text}</StyledTooltip>;
 
-const CustomTooltip = ({ children, text, delay, ...rest }: any) => {
+const CustomTooltip = ({ children, text, delay, placement, ...rest }: any) => {
   return (
     <Whisper
       trigger="hover"
-      delay={delay || 300}
+      delay={delay || 500}
       speaker={tooltip(text)}
-      placement={rest.placement || 'top'}
+      placement={placement || 'top'}
       {...rest}
     >
       {children}
