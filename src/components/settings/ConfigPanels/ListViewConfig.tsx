0c4ae075426a1606e6361c04c86465fe6a39307d
jeffvli <jeffvictorli@gmail.com>
2021-10-26 02:10:01 -0700

    Add drag/drop component for column selector

--- src/components/settings/ConfigPanels/ListViewConfig.tsx
+++ src/components/settings/ConfigPanels/ListViewConfig.tsx
@@ -1,58 +1,188 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid/non-secure';
 import settings from 'electron-settings';
-import { TagPicker, ControlLabel } from 'rsuite';
+import { TagPicker, ControlLabel, Panel } from 'rsuite';
 import { StyledInputNumber } from '../../shared/styled';
+import ListViewTable from '../../viewtypes/ListViewTable';
+import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
+import {
+  setIsDragging,
+  setRangeSelected,
+  toggleRangeSelected,
+  toggleSelected,
+} from '../../../redux/multiSelectSlice';
+import { ColumnList, moveToIndex, setColumnList } from '../../../redux/configSlice';
+
+const columnSelectorColumns = [
+  {
+    id: '#',
+    dataKey: 'index',
+    alignment: 'center',
+    resizable: false,
+    width: 50,
+    label: '#',
+  },
+  {
+    id: 'Column',
+    dataKey: 'label',
+    alignment: 'left',
+    resizable: false,
+    flexGrow: 2,
+    label: 'Column',
+  },
+  {
+    id: 'Resizable',
+    dataKey: 'columnResizable',
+    alignment: 'left',
+    resizable: false,
+    width: 100,
+    label: 'Resizable',
+  },
+];
 
 const ListViewConfig = ({ defaultColumns, columnPicker, columnList, settingsConfig }: any) => {
+  const dispatch = useAppDispatch();
+  const playQueue = useAppSelector((state) => state.playQueue);
+  const multiSelect = useAppSelector((state) => state.multiSelect);
+  const config = useAppSelector((state) => state.config);
+  const [selectedColumns, setSelectedColumns] = useState([]);
+  const columnListType = settingsConfig.columnList.split('List')[0];
+
+  useEffect(() => {
+    const cols = config.lookAndFeel.listView[columnListType].columns.map((col: any) => {
+      return col.label;
+    });
+
+    setSelectedColumns(cols);
+
+    settings.setSync(
+      settingsConfig.columnList,
+      config.lookAndFeel.listView[columnListType].columns
+    );
+  }, [columnListType, config.lookAndFeel.listView, settingsConfig.columnList]);
+
+  let timeout: any = null;
+  const handleRowClick = (e: any, rowData: any) => {
+    if (timeout === null) {
+      timeout = window.setTimeout(() => {
+        timeout = null;
+
+        if (e.ctrlKey) {
+          dispatch(toggleSelected(rowData));
+        } else if (e.shiftKey) {
+          dispatch(setRangeSelected(rowData));
+          dispatch(toggleRangeSelected(config.lookAndFeel.listView[columnListType].columns));
+        }
+      }, 100);
+    }
+  };
+
+  const handleDragEnd = (listType: ColumnList) => {
+    if (multiSelect.isDragging) {
+      dispatch(
+        moveToIndex({
+          entries: multiSelect.selected,
+          moveBeforeId: multiSelect.currentMouseOverId,
+          listType,
+        })
+      );
+      dispatch(setIsDragging(false));
+    }
+  };
+
   return (
     <div style={{ width: '100%' }}>
-      <br />
-      <TagPicker
-        data={columnPicker}
-        defaultValue={defaultColumns}
-        style={{ width: '500px' }}
-        onChange={(e) => {
-          const columns: any[] = [];
+      <div>
+        <Panel bordered bodyFill>
+          <TagPicker
+            data={columnPicker}
+            defaultValue={defaultColumns}
+            value={selectedColumns}
+            style={{ width: '100%' }}
+            onChange={(e) => {
+              const columns: any[] = [];
+              if (e) {
+                e.forEach((selected: string) => {
+                  const alreadySelectedColumn = config.lookAndFeel.listView[
+                    columnListType
+                  ].columns.find((column: any) => column.label === selected);
 
-          if (e) {
-            e.map((selected: string) => {
-              const selectedColumn = columnList.find((column: any) => column.label === selected);
-              if (selectedColumn) {
-                return columns.push(selectedColumn.value);
+                  if (alreadySelectedColumn) {
+                    return columns.push(alreadySelectedColumn);
+                  }
+
+                  const selectedColumn = columnList.find(
+                    (column: any) => column.label === selected
+                  );
+
+                  if (selectedColumn) {
+                    return columns.push({ ...selectedColumn.value, uniqueId: nanoid() });
+                  }
+
+                  return null;
+                });
               }
 
-              return null;
-            });
-          }
+              const cleanColumns = columns.map((col) => {
+                const { uniqueId, ...rest } = col;
+                return rest;
+              });
 
-          settings.setSync(settingsConfig.columnList, columns);
-        }}
-        labelKey="label"
-        valueKey="label"
-      />
-      <div style={{ marginTop: '20px' }}>
+              dispatch(setColumnList({ listType: columnListType, entries: columns }));
+              settings.setSync(settingsConfig.columnList, cleanColumns);
+            }}
+            labelKey="label"
+            valueKey="label"
+          />
+
+          <ListViewTable
+            data={config.lookAndFeel.listView[columnListType].columns || []}
+            height={300}
+            handleRowClick={handleRowClick}
+            handleRowDoubleClick={() => {}}
+            handleDragEnd={() => handleDragEnd(columnListType)}
+            columns={columnSelectorColumns}
+            rowHeight={35}
+            fontSize={12}
+            listType="column"
+            cacheImages={{ enabled: false }}
+            playQueue={playQueue}
+            multiSelect={multiSelect}
+            isModal={false}
+            miniView={false}
+            dnd
+            disableContextMenu
+            config={{ option: columnListType, columnList }}
+            virtualized
+          />
+        </Panel>
+      </div>
+
+      <br />
+      <div>
         <ControlLabel>Row height</ControlLabel>
         <StyledInputNumber
-          defaultValue={String(settings.getSync(settingsConfig.rowHeight)) || '0'}
+          defaultValue={Number(settings.getSync(settingsConfig.rowHeight)) || 0}
           step={1}
           min={15}
           max={250}
           width={150}
-          onChange={(e: any) => {
-            settings.setSync(settingsConfig.rowHeight, e);
+          onChange={(e: number) => {
+            settings.setSync(settingsConfig.rowHeight, Number(e));
           }}
         />
       </div>
-      <div style={{ marginTop: '20px' }}>
+      <br />
+      <div>
         <ControlLabel>Font size</ControlLabel>
         <StyledInputNumber
-          defaultValue={String(settings.getSync(settingsConfig.fontSize)) || '0'}
+          defaultValue={Number(settings.getSync(settingsConfig.fontSize)) || 0}
           step={0.5}
           min={1}
           max={100}
           width={150}
-          onChange={(e: any) => {
-            settings.setSync(settingsConfig.fontSize, e);
+          onChange={(e: number) => {
+            settings.setSync(settingsConfig.fontSize, Number(e));
           }}
         />
       </div>
--- src/redux/configSlice.ts
+++ src/redux/configSlice.ts
@@ -1,14 +1,79 @@
+import { nanoid } from 'nanoid/non-secure';
+import _ from 'lodash';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import settings from 'electron-settings';
+import { mockSettings } from '../shared/mockSettings';
+import { moveSelectedToIndex } from '../shared/utils';
+
+const parsedSettings: any = process.env.NODE_ENV === 'test' ? mockSettings : settings.getSync();
 
 export interface ConfigPage {
   active: {
     tab: string;
   };
+  lookAndFeel: {
+    listView: {
+      music: { columns: any; rowHeight: number; fontSize: number };
+      album: { columns: any; rowHeight: number; fontSize: number };
+      playlist: { columns: any; rowHeight: number; fontSize: number };
+      artist: { columns: any; rowHeight: number; fontSize: number };
+      genre: { columns: any; rowHeight: number; fontSize: number };
+      mini: { columns: any; rowHeight: number; fontSize: number };
+    };
+  };
 }
+
+export type ColumnList = 'music' | 'album' | 'playlist' | 'artist' | 'genre' | 'mini';
 
 const initialState: ConfigPage = {
   active: {
     tab: 'playback',
+  },
+  lookAndFeel: {
+    listView: {
+      music: {
+        columns: parsedSettings.musicListColumns!.map((col: any) => {
+          return { ...col, uniqueId: nanoid() };
+        }),
+        rowHeight: Number(parsedSettings.musicListRowHeight),
+        fontSize: Number(parsedSettings.musicListFontSize),
+      },
+      album: {
+        columns: parsedSettings.albumListColumns!.map((col: any) => {
+          return { ...col, uniqueId: nanoid() };
+        }),
+        rowHeight: Number(parsedSettings.albumListRowHeight),
+        fontSize: Number(parsedSettings.albumListFontSize),
+      },
+      playlist: {
+        columns: parsedSettings.playlistListColumns!.map((col: any) => {
+          return { ...col, uniqueId: nanoid() };
+        }),
+        rowHeight: Number(parsedSettings.playlistListRowHeight),
+        fontSize: Number(parsedSettings.playlistListFontSize),
+      },
+      artist: {
+        columns: parsedSettings.artistListColumns!.map((col: any) => {
+          return { ...col, uniqueId: nanoid() };
+        }),
+        rowHeight: Number(parsedSettings.artistListRowHeight),
+        fontSize: Number(parsedSettings.artistListFontSize),
+      },
+      genre: {
+        columns: parsedSettings.genreListColumns!.map((col: any) => {
+          return { ...col, uniqueId: nanoid() };
+        }),
+        rowHeight: Number(parsedSettings.genreListRowHeight),
+        fontSize: Number(parsedSettings.genreListFontSize),
+      },
+      mini: {
+        columns: parsedSettings.miniListColumns!.map((col: any) => {
+          return { ...col, uniqueId: nanoid() };
+        }),
+        rowHeight: Number(parsedSettings.miniListRowHeight),
+        fontSize: Number(parsedSettings.miniListFontSize),
+      },
+    },
   },
 };
 
@@ -19,8 +84,27 @@ const configSlice = createSlice({
     setActive: (state, action: PayloadAction<{ tab: string }>) => {
       state.active.tab = action.payload.tab;
     },
+
+    setColumnList: (state, action: PayloadAction<{ listType: ColumnList; entries: any }>) => {
+      state.lookAndFeel.listView[action.payload.listType].columns = action.payload.entries;
+    },
+
+    moveToIndex: (
+      state,
+      action: PayloadAction<{
+        entries: any;
+        moveBeforeId: string;
+        listType: 'music' | 'album' | 'playlist' | 'artist' | 'genre' | 'mini';
+      }>
+    ) => {
+      state.lookAndFeel.listView[action.payload.listType].columns = moveSelectedToIndex(
+        state.lookAndFeel.listView[action.payload.listType].columns,
+        action.payload.entries,
+        action.payload.moveBeforeId
+      );
+    },
   },
 });
 
-export const { setActive } = configSlice.actions;
+export const { setActive, setColumnList, moveToIndex } = configSlice.actions;
 export default configSlice.reducer;
