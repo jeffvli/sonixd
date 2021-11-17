6722852d34da97f78e0092b710fa25a92d5157f6
jeffvli <jeffvictorli@gmail.com>
2021-11-16 18:37:08 -0800

    Update login/logout for jellyfin

--- src/components/settings/DisconnectButton.tsx
+++ src/components/settings/DisconnectButton.tsx
@@ -2,20 +2,27 @@ import React from 'react';
 import settings from 'electron-settings';
 import { StyledButton } from '../shared/styled';
 
-const DisconnectButton = () => {
-  const handleDisconnect = () => {
-    localStorage.removeItem('server');
-    localStorage.removeItem('username');
-    localStorage.removeItem('salt');
-    localStorage.removeItem('hash');
+export const handleDisconnect = () => {
+  localStorage.removeItem('server');
+  localStorage.removeItem('username');
+  localStorage.removeItem('userId');
+  localStorage.removeItem('password');
+  localStorage.removeItem('salt');
+  localStorage.removeItem('hash');
+  localStorage.removeItem('token');
 
-    settings.setSync('server', '');
-    settings.setSync('serverBase64', '');
-    settings.setSync('username', '');
-    settings.setSync('salt', '');
-    settings.setSync('hash', '');
-    window.location.reload();
-  };
+  settings.setSync('server', '');
+  settings.setSync('serverBase64', '');
+  settings.setSync('username', '');
+  settings.setSync('userId', '');
+  settings.setSync('password', '');
+  settings.setSync('salt', '');
+  settings.setSync('hash', '');
+  settings.setSync('token', '');
+  window.location.reload();
+};
+
+const DisconnectButton = () => {
   return (
     <StyledButton onClick={handleDisconnect} size="sm">
       Disconnect
