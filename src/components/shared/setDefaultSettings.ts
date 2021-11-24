c523ed5f2645c54ea09be9a5b57fe6bd12121efd
jeffvli <jeffvictorli@gmail.com>
2021-11-24 00:19:03 -0800

    Add default serverType setting

--- src/components/shared/setDefaultSettings.ts
+++ src/components/shared/setDefaultSettings.ts
@@ -2,6 +2,10 @@ import settings from 'electron-settings';
 import path from 'path';
 
 const setDefaultSettings = (force: boolean) => {
+  if (force || !settings.hasSync('serverType')) {
+    settings.setSync('serverType', 'subsonic');
+  }
+
   if (force || !settings.hasSync('legacyAuth')) {
     settings.setSync('legacyAuth', false);
   }
