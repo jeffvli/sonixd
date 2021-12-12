0861000d514f0fe8bc93eb7ee06614016928514e
jeffvli <jeffvictorli@gmail.com>
2021-12-11 20:25:05 -0800

    Add artist filter, clean up other - Return filtered data from each step - Set data dynamically by selected filters - Add separate check picker clear buttons

--- src/hooks/useAdvancedFilter.ts
+++ src/hooks/useAdvancedFilter.ts
@@ -1,19 +1,13 @@
 import { useState, useEffect } from 'react';
 import _ from 'lodash';
-
-interface AdvancedFilters {
-  enabled: boolean;
-  properties: {
-    starred: boolean;
-    genre: {
-      list: any[];
-      type: 'or' | 'and';
-    };
-  };
-}
+import { AdvancedFilters } from '../redux/albumSlice';
 
 const useAdvancedFilter = (data: any[], filters: AdvancedFilters) => {
   const [filteredData, setFilteredData] = useState<any[]>([]);
+  const [byStarredData, setByStarredData] = useState<any[]>([]);
+  const [byGenreData, setByGenreData] = useState<any[]>([]);
+  const [byArtistData, setByArtistData] = useState<any[]>([]);
+
   const [filterProps, setFilterProps] = useState(filters);
 
   useEffect(() => {
@@ -48,13 +42,38 @@ const useAdvancedFilter = (data: any[], filters: AdvancedFilters) => {
             })
           : filteredByStarred;
 
-      setFilteredData(_.compact(_.uniqBy(filteredByGenres, 'uniqueId')));
+      const artistRegex = new RegExp(filterProps.properties?.artist?.list.join('|'), 'i');
+
+      const filteredByArtists =
+        filterProps.properties.artist.list.length > 0
+          ? (filteredByGenres || []).filter((entry) => {
+              const entryArtistIds = _.map(entry.artist, 'id');
+
+              if (filterProps.properties.artist.type === 'or') {
+                return entryArtistIds.some((artistId) => artistId.match(artistRegex));
+              }
+
+              const matches = [];
+              for (let i = 0; i < filterProps.properties.artist.list.length; i += 1) {
+                if (entryArtistIds.includes(filterProps.properties.artist.list[i])) {
+                  matches.push(entry);
+                }
+              }
+
+              return matches.length === filterProps.properties.artist.list.length;
+            })
+          : filteredByGenres;
+
+      setByStarredData(_.compact(_.uniqBy(filteredByStarred, 'uniqueId')));
+      setByGenreData(_.compact(_.uniqBy(filteredByGenres, 'uniqueId')));
+      setByArtistData(_.compact(_.uniqBy(filteredByArtists, 'uniqueId')));
+      setFilteredData(_.compact(_.uniqBy(filteredByArtists, 'uniqueId')));
     } else {
       setFilteredData(data);
     }
   }, [data, filterProps, filters]);
 
-  return filteredData;
+  return { filteredData, byStarredData, byGenreData, byArtistData };
 };
 
 export default useAdvancedFilter;
