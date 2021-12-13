d1d4e85df8bd42f39886b5059f8dea640e1ca221
gelaechter <30231932+gelaechter@users.noreply.github.com>
2021-12-13 22:36:27 +0100

    change apiController to accept funkwhale serverType

--- src/api/controller.ts
+++ src/api/controller.ts
@@ -62,7 +62,7 @@ import {
   updatePlaylistSongsLg as jfUpdatePlaylingSongsLg,
   updatePlaylist as jfUpdatePlaylist,
 } from './jellyfinApi';
-import { APIEndpoints, ServerType } from '../types';
+import { APIEndpoints, Server, ServerType } from '../types';
 
 // prettier-ignore
 const endpoints = [
@@ -108,7 +108,11 @@ export const apiController = async (options: {
   args?: any;
 }) => {
   const selectedEndpoint = endpoints.find((e) => e.id === options.endpoint);
-  const selectedEndpointFn = selectedEndpoint!.endpoint[options.serverType];
+  // If the serverType is funkwhale call the subsonic endpoint
+  const selectedEndpointFn =
+    options.serverType === Server.Funkwhale
+      ? selectedEndpoint!.endpoint[Server.Subsonic]
+      : selectedEndpoint!.endpoint[options.serverType];
 
   if (!selectedEndpointFn || !selectedEndpoint) {
     return notifyToast('warning', `[${options.endpoint}] not available`);
