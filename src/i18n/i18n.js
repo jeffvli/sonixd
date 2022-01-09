0d414ad65f0cdd9c3fef4f952ac4afc4331d9929
gelaechter <30231932+gelaechter@users.noreply.github.com>
2022-01-09 02:22:10 +0100

    Add i18next, react-i18next, i18next-parser and configs

---
+++ src/i18n/i18n.js
@@ -0,0 +1,39 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+// the translations
+// (tip move them in a JSON file and import them,
+// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
+const de = require('./locales/de.json');
+const en = require('./locales/en.json');
+
+const resources = {
+  en: { translation: en },
+  de: { translation: de },
+};
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    fallbackLng: 'en', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
+    // if you're using a language detector, do not define the lng option
+
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  });
+
+export default i18n;
+
+export const Languages = [
+  {
+    label: 'English',
+    value: 'en',
+  },
+  {
+    label: 'Deutsch',
+    value: 'de',
+  },
+];
