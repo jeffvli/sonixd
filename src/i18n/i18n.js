a98f41992c394544cc26e485c1156119fb193cb8
gelaechter <30231932+gelaechter@users.noreply.github.com>
2022-01-16 00:46:16 +0100

    Add mockSettings to i18n.js

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,6 +1,7 @@
 import settings from 'electron-settings';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import { mockSettings } from '../shared/mockSettings';
 
 // the translations
 // (tip move them in a JSON file and import them,
@@ -17,7 +18,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: settings.getSync('language'),
+    lng: process.env.NODE_ENV === 'test' ? mockSettings.language : settings.getSync('language'),
     fallbackLng: 'en', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
