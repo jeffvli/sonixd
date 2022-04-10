202fed044da1561a64621085c2841873388bda57
jeffvli <jeffvictorli@gmail.com>
2022-04-09 19:43:53 -0700

    Add custom slider component

---
+++ src/components/slider/Slider.tsx
@@ -0,0 +1,106 @@
+import React, { useMemo, useState } from 'react';
+import ReactSlider, { ReactSliderProps } from 'react-slider';
+import styled from 'styled-components';
+import format from 'format-duration';
+
+const StyledSlider = styled<any>(ReactSlider)`
+  width: 100%;
+  height: 25px;
+  outline: none;
+
+  .thumb {
+    opacity: 1;
+    top: 37%;
+
+    &:after {
+      content: attr(data-tooltip);
+      top: -25px;
+      left: -15px;
+      color: ${(props) => props.theme.colors.tooltip.color};
+      background: ${(props) => props.theme.colors.tooltip.background};
+      border-radius: 4px;
+      padding: 2px 6px;
+      white-space: nowrap;
+      position: absolute;
+      display: ${(props) => (props.isDragging ? 'block' : 'none')};
+    }
+
+    &:focus-visible {
+      outline: none;
+      height: 13px;
+      width: 13px;
+      border: 1px ${(props) => props.theme.colors.primary} solid;
+      border-radius: 100%;
+      text-align: center;
+      background-color: #ffffff;
+      transform: translateY(-3px);
+    }
+  }
+
+  .track-0 {
+    background: ${(props) => props.isDragging && props.theme.colors.primary};
+  }
+
+  .track {
+    top: 37%;
+  }
+
+  &:hover {
+    .track-0 {
+      background: ${(props) => (props.index === 1 ? '#3c3f43' : props.theme.colors.primary)};
+    }
+  }
+`;
+
+const StyledTrack = styled.div<any>`
+  top: 0;
+  bottom: 0;
+  height: 5px;
+  background: ${(props) =>
+    props.index === 1
+      ? props.theme.colors.slider.background
+      : props.theme.colors.slider.progressBar};
+`;
+
+const StyledThumb = styled.div``;
+
+const MemoizedThumb = ({ props, state }: any) => {
+  const { value } = state;
+  const time = useMemo(() => format(value * 1000), [value]);
+
+  return <StyledThumb {...props} data-tooltip={time} />;
+};
+
+// eslint-disable-next-line react/destructuring-assignment
+const Track = (props: any, state: any) => <StyledTrack {...props} index={state.index} />;
+const Thumb = (props: any, state: any) => (
+  <MemoizedThumb tabIndex={0} props={props} state={state} />
+);
+
+const Slider = ({ ...rest }: ReactSliderProps) => {
+  const [isDragging, setIsDragging] = useState(false);
+
+  return (
+    <StyledSlider
+      {...rest}
+      defaultValue={0}
+      renderTrack={Track}
+      renderThumb={Thumb}
+      isDragging={isDragging}
+      onBeforeChange={(e: number, index: number) => {
+        if (rest.onBeforeChange) {
+          rest.onBeforeChange(e, index);
+        }
+        setIsDragging(true);
+      }}
+      onAfterChange={(e: number, index: number) => {
+        if (rest.onAfterChange) {
+          rest.onAfterChange(e, index);
+        }
+        setIsDragging(false);
+      }}
+    />
+  );
+};
+
+export default Slider;
