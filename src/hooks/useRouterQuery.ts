53ef540ffd8ba88e182d6ff0afc1a0a5de6f8743
jeffvli <jeffvictorli@gmail.com>
2022-04-14 00:10:35 -0700

    Remove legacy files

--- src/hooks/useRouterQuery.ts
+++
@@ -1,7 +0,0 @@
-import { useLocation } from 'react-router-dom';
-
-const useRouterQuery = () => {
-  return new URLSearchParams(useLocation().search);
-};
-
-export default useRouterQuery;
--- src/shared/config.ts
+++
@@ -1,9 +0,0 @@
-export interface Config {
-  serverUrl: string;
-}
-
-const { env } = window as any;
-
-export const config: Config = {
-  serverUrl: env?.SERVER_URL || '',
-};
