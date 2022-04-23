f40c14727a91d9f29b43e0235c4c8b9d8bb88e20
jeffvli <jeffvictorli@gmail.com>
2022-04-22 19:49:34 -0700

    Add redux devtools

--- src/main/main.ts
+++ src/main/main.ts
@@ -40,7 +40,7 @@ if (isDevelopment) {
 const installExtensions = async () => {
   const installer = require('electron-devtools-installer');
   const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-  const extensions = ['REACT_DEVELOPER_TOOLS'];
+  const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
 
   return installer
     .default(
