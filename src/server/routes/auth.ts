a9836293de81f381ced13a867d443284565c1f95
jeffvli <jeffvictorli@gmail.com>
2022-05-03 20:19:29 -0700

    Add server ping route

--- src/server/routes/auth.ts
+++ src/server/routes/auth.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import passport from 'passport';
 
 import { authenticateLocal } from '../middleware';
+import packageJson from '../package.json';
 import { authService } from '../services';
 import { getSuccessResponse } from '../utils';
 
@@ -30,6 +31,19 @@ authRouter.post('/logout', authenticateLocal, async (req, res) => {
   });
 
   return res.sendStatus(204);
+});
+
+authRouter.get('/ping', async (_req, res) => {
+  return res.status(200).json(
+    getSuccessResponse({
+      statusCode: 200,
+      data: {
+        name: packageJson.name,
+        description: packageJson.description,
+        version: packageJson.version,
+      },
+    })
+  );
 });
 
 export default authRouter;
