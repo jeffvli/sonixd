ac5931f8cb1c225517ac69403d298588cc17f58b
jeffvli <jeffvictorli@gmail.com>
2022-05-20 01:55:29 -0700

    Add generic error messages

--- src/server/utils/api-error.ts
+++ src/server/utils/api-error.ts
@@ -9,31 +9,40 @@ class ApiError extends Error {
   }
 
   static badRequest(message: string) {
-    return new ApiError({ statusCode: 400, message });
+    return new ApiError({
+      statusCode: 400,
+      message: message || 'Bad request.',
+    });
   }
 
   static unauthorized(message: string) {
-    return new ApiError({ statusCode: 401, message });
+    return new ApiError({
+      statusCode: 401,
+      message: message || 'Unauthorized.',
+    });
   }
 
   static forbidden(message: string) {
-    return new ApiError({ statusCode: 403, message });
+    return new ApiError({ statusCode: 403, message: message || 'Forbidden.' });
   }
 
   static notFound(message: string) {
-    return new ApiError({ statusCode: 404, message });
+    return new ApiError({ statusCode: 404, message: message || 'Not found.' });
   }
 
   static conflict(message: string) {
-    return new ApiError({ statusCode: 409, message });
+    return new ApiError({ statusCode: 409, message: message || 'Conflict.' });
   }
 
   static gone(message: string) {
-    return new ApiError({ statusCode: 410, message });
+    return new ApiError({ statusCode: 410, message: message || 'Gone.' });
   }
 
   static internal(message: string) {
-    return new ApiError({ statusCode: 500, message });
+    return new ApiError({
+      statusCode: 500,
+      message: message || 'Internal error.',
+    });
   }
 }
 
