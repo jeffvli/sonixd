e7c3070e175908e0190b6004e1564745b230ef31
jeffvli <jeffvictorli@gmail.com>
2022-05-22 15:22:21 -0700

    Update linting to sort keys, no error on unused

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,69 +1,87 @@
 module.exports = {
-  extends: 'erb',
-  plugins: ['import'],
+  extends: ['erb', 'plugin:typescript-sort-keys/recommended'],
+  parserOptions: {
+    createDefaultProgram: true,
+    ecmaVersion: 2020,
+    project: './tsconfig.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['import', 'sort-keys-fix'],
   rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+
+    '@typescript-eslint/no-non-null-assertion': 'off',
+
     // A temporary hack related to IDE not resolving correct package.json
     'import/no-extraneous-dependencies': 'off',
+
     'import/no-unresolved': 'error',
-    // Since React 17 and typescript 4.1 you can safely disable the rule
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    'import/prefer-default-export': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'jsx-a11y/media-has-caption': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'jsx-a11y/click-events-have-key-events': 'off',
-    'jsx-a11y/interactive-supports-focus': 'off',
-    'no-console': 'off',
+
     'import/order': [
       'error',
       {
+        alphabetize: {
+          caseInsensitive: true,
+          order: 'asc',
+        },
         groups: ['builtin', 'external', 'internal', ['parent', 'sibling']],
+        'newlines-between': 'never',
         pathGroups: [
           {
-            pattern: 'react',
             group: 'external',
+            pattern: 'react',
             position: 'before',
           },
         ],
         pathGroupsExcludedImportTypes: ['react'],
-        'newlines-between': 'always',
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
       },
     ],
+
+    'import/prefer-default-export': 'off',
+
+    'jsx-a11y/click-events-have-key-events': 'off',
+
+    'jsx-a11y/interactive-supports-focus': 'off',
+
+    'jsx-a11y/media-has-caption': 'off',
+
+    'no-await-in-loop': 'off',
+
+    'no-console': 'off',
+
+    'no-nested-ternary': 'off',
+
+    'no-restricted-syntax': 'off',
+
+    'react/jsx-props-no-spreading': 'off',
+
     'react/jsx-sort-props': [
       'error',
       {
         callbacksLast: true,
-        shorthandFirst: true,
-        shorthandLast: false,
         ignoreCase: false,
         noSortAlphabetically: false,
         reservedFirst: true,
+        shorthandFirst: true,
+        shorthandLast: false,
       },
     ],
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
-    createDefaultProgram: true,
+    // Since React 17 and typescript 4.1 you can safely disable the rule
+    'react/react-in-jsx-scope': 'off',
+    'sort-keys-fix/sort-keys-fix': 'warn',
   },
   settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
     'import/resolver': {
       // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
       node: {},
+      typescript: {},
       webpack: {
         config: require.resolve('./.erb/configs/webpack.config.eslint.ts'),
       },
-      typescript: {},
-    },
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
   },
 };
