33ebd26c98fdfb04a5a6acf8acc5fa6c395381fd
jeffvli <jeffvictorli@gmail.com>
2022-06-06 19:33:40 -0700

    Add prisma retry middleware

--- src/server/lib/prisma.ts
+++ src/server/lib/prisma.ts
@@ -11,3 +11,22 @@ export const exclude = <T, Key extends keyof T>(
   }
   return resultSet;
 };
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+prisma.$use(async (params, next) => {
+  const maxRetries = 5;
+  let retries = 0;
+
+  do {
+    try {
+      const result = await next(params);
+      return result;
+    } catch (err) {
+      retries += 1;
+      return sleep(500);
+    }
+  } while (retries < maxRetries);
+});
