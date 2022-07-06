44cb9a9b88dc285e8c564b99864b69427ae689d2
jeffvli <jeffvictorli@gmail.com>
2022-07-05 18:33:17 -0700

    Remove python player

--- src/player/server.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, request
-from flask_socketio import SocketIO, send, emit
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-app.config["SECRET_KEY"] = "secret!"
-socketio = SocketIO(app, cors_allowed_origins="*")
