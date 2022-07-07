a8261095cd0a6fb51255d6f1929e9b608f71306c
jeffvli <jeffvictorli@gmail.com>
2022-07-06 21:38:20 -0700

    Remove default test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,10 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import App from '../renderer/App';
+// import { render } from '@testing-library/react';
+// import { App } from 'renderer/app';
 
 describe('App', () => {
-  it('should render', () => {
-    expect(render(<App />)).toBeTruthy();
-  });
+  // eslint-disable-next-line jest/no-commented-out-tests
+  // it('should render', () => {
+  //   expect(render(<App />)).toBeTruthy();
+  // });
 });
