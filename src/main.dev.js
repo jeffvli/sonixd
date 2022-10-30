b191402ba50bb043d4aa89adc9addcc000043685
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2022-10-29 23:34:40 -0400

    fix exit from tray

--- src/main.dev.js
+++ src/main.dev.js
@@ -435,6 +435,7 @@ const createWindow = async () => {
 
   mainWindow.on('close', (event) => {
     if (!exitFromTray && store.getState().config.window.exitToTray) {
+      exitFromTray = true;
       event.preventDefault();
       mainWindow.hide();
     }
