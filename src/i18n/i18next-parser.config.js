c5be7a09acea3964801ba1afa159575923b56890
SKEIDs <47618415+SKEIDs@users.noreply.github.com>
2023-07-17 03:41:47 +0900

    Update some config

--- src/i18n/i18next-parser.config.js
+++ src/i18n/i18next-parser.config.js
@@ -44,7 +44,7 @@ module.exports = {
   lineEnding: 'auto',
   // Control the line ending. See options at https://github.com/ryanve/eol
 
-  locales: ['en', 'de', 'zhcn', 'es', 'si', 'it', 'tr'],
+  locales: ['en', 'de', 'zhcn', 'es', 'si', 'it', 'tr', 'ja'],
   // An array of the locales in your applications
 
   namespaceSeparator: false,
